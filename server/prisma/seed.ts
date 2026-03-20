50a818afb2dbc3458c5b9f27fdd46f46fc958a14
HamoGit7 <suia.s23@gmail.com>
2026-03-20 15:45:20 +0900

    グラフ追加＆レイアウト変更

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,52 +1,47 @@
 // seed:APIとは別のDB準備専用プログラム(SQLを生成→DBへ送信→結果を受け取る)
 
-import { PrismaClient } from "../src/generated/prisma"
+import { PrismaClient, CategoryType } from "../src/generated/prisma";
 
 const prisma = new PrismaClient(); // 実体
 
 // 非同期処理をまとめるメイン関数(Promiseを返す)
 async function main() {
+  const categories = await prisma.category.createMany({
+    data: [
+      { id: "c_food", name: "食費", type: CategoryType.expense },
+      { id: "c_travel", name: "交通費", type: CategoryType.expense },
+      { id: "c_salary", name: "給与", type: CategoryType.expense },
+      { id: "c_sideinc", name: "副収入", type: CategoryType.expense },
+      { id: "c_other", name: "その他", type: CategoryType.expense },
+    ],
+    skipDuplicates: true,
+  });
 
-    //とりあえずカテゴリ（最小）
-    const categories = await prisma.category.createMany({
-        data:[
-            {id:"c_food",name:"食費",type:"expense"},
-            {id:"c_travel",name:"交通費",type:"expense"},
-            {id:"c_salary",name:"給与",type:"income"},
-            {id:"c_sideinc",name:"副収入",type:"income"}
-            // {id:"c_entmt",name:"娯楽",type:"both"},
-            // {id:"c_other",name:"その他",type:"both"},
-        ],
-        skipDuplicates: true,
-      
+  // 1件だけ取引を入れて動作確認（カテゴリIDが必要なので取得）
+  const food = await prisma.category.findFirst({ where: { name: "食費" } });
+
+  if (food) {
+    await prisma.transaction.create({
+      data: {
+        date: new Date("2026-02-01"),
+        type: "expense",
+        title: "",
+        amount: 900,
+        categoryId: food.id,
+      },
     });
+  }
 
-    // 1件だけ取引を入れて動作確認（カテゴリIDが必要なので取得）
-    const food = await prisma.category.findFirst({ where: {name: "食費"}});
-
-    if (food) {
-        await prisma.transaction.create({
-            data:{
-                date:new Date("2026-02-01"),
-                type:"expense",
-                title:"",
-                amount:900,
-                categoryId:food.id
-            },
-        });
-    }
-
-    console.log("seed done",{ categories }); // 終了ログ
-
+  console.log("seed done", { categories }); // 終了ログ
 }
 
 // Promiseが失敗したときの保険
 main()
-    .catch((e) => { // エラーをキャッチしてエラーを表示する
-        console.error(e);
-        process.exit(1); // 正常終了:0、異常終了:1
-    })
-    .finally(async () => {
-        await prisma.$disconnect(); // 成功でも失敗でもDB接続を閉じる
-    });
-
+  .catch((e) => {
+    // エラーをキャッチしてエラーを表示する
+    console.error(e);
+    process.exit(1); // 正常終了:0、異常終了:1
+  })
+  .finally(async () => {
+    await prisma.$disconnect(); // 成功でも失敗でもDB接続を閉じる
+  });
